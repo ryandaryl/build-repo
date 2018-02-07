336e5ddabe0711738419b0d7cababa11922eccf2
ryandaryl <ryandarylmills@gmail.com>
2018-02-07 02:28:17 +0000

    No commit message.

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 
 import os
-from flask import Flask, render_template
-app = Flask(__name__, template_folder="")
+from flask import Flask, send_from_directory, render_template
+app = Flask(__name__, template_folder='')
+
+@app.route('/<path:filename>')
+def serve_static(filename):
+    return send_from_directory('.', filename)
 
 @app.route("/")
-def hello():
-    return render_template("index.html")
+def index():
+    return render_template('index.html')
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
