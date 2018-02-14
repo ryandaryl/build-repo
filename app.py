6155a90cb32a3f8d54b2b0ec3d443c5747cc4d52
ryandaryl <ryandarylmills@gmail.com>
2018-02-14 01:44:53 +0000

    No commit message.

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, Blueprint
+from flask.ext import restful
+
+class HelloWorld(restful.Resource):
+    def get(self):
+        return {'hello': 'world'}
+
+blueprint = Blueprint('my_blueprint', __name__)
+
+api = restful.Api(blueprint, prefix="/blueprint")
+api.add_resource(HelloWorld, "/helloworld")
+
+app = Flask(__name__)
+app.register_blueprint(blueprint)
+
+if __name__ == '__main__':
+    app.run(debug=True)
