3bfde5eb13a45b23b206fb2d4868965b07c70e42
ryandaryl <ryandarylmills@gmail.com>
2018-02-21 09:36:59 +0000

    No commit message.

--- project/platform/section_b/__init__.py
+++
@@ -1,9 +0,0 @@
-import flask_restful
-from project import blueprints
-
-api = flask_restful.Api(blueprints[__name__], prefix="/blueprint2")
-
-@api.route('/helloworld')
-class HelloWorld(flask_restful.Resource):
-    def get(self):
-        return {'hello2': 'world2'}
